8fee65bc2013a65dc975fa5735fb95717aab2c1c
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-04-08 17:31:58 +0900

    Feat: Implement modal popup on random movie info button clicks

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -8,11 +8,16 @@ import {
   Buttons,
 } from './Banner';
 import Input from './Input';
+import MovieModal from '../MovieModal';
 import { getRandomMovie } from '../../apis/apis';
 import { BsFillPlayFill } from 'react-icons/bs';
 
 function Banner() {
+  // 영화 상세 정보 state
   const [movie, setMovie]: any = useState({});
+  // 영화 상세정보 모달 팝업 state
+  const [openModal, setOpenModal] = useState(false);
+  // 비디오 배너의 오픈 상태 state
   const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
@@ -32,39 +37,50 @@ function Banner() {
     setIsClicked((prev) => !prev);
   };
 
+  const handleInfoButton = () => {
+    setOpenModal((prev) => !prev);
+  };
+
   if (!isClicked) {
     return (
-      <Header
-        movie={movie}
-        style={{
-          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-          backgroundPosition: 'top center',
-          backgroundSize: 'cover',
-        }}
-      >
-        <Content>
-          <div className="content-position">
-            <h1>{movie.title || movie.name || movie.original_name}</h1>
-            <Buttons>
-              <button className="play" onClick={handlePlayButton}>
-                <BsFillPlayFill />
-                Play
-              </button>
-              <button className="info">영화 정보</button>
-            </Buttons>
+      <>
+        <Header
+          movie={movie}
+          style={{
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+            backgroundPosition: 'top center',
+            backgroundSize: 'cover',
+          }}
+        >
+          <Content>
+            <div className="content-position">
+              <h1>{movie.title || movie.name || movie.original_name}</h1>
+              <Buttons>
+                <button className="play" onClick={handlePlayButton}>
+                  <BsFillPlayFill />
+                  Play
+                </button>
+                <button className="info" onClick={handleInfoButton}>
+                  영화 정보
+                </button>
+              </Buttons>
 
-            {truncateTexts(movie.overview, 100)
-              ?.split('. ')
-              .map((line: string, index: number) => (
-                <p key={index}>{line}</p>
-              ))}
+              {truncateTexts(movie.overview, 100)
+                ?.split('. ')
+                .map((line: string, index: number) => (
+                  <p key={index}>{line}</p>
+                ))}
 
-            <div className="fade-bottom" />
-          </div>
-        </Content>
+              <div className="fade-bottom" />
+            </div>
+          </Content>
 
-        <Input />
-      </Header>
+          <Input />
+        </Header>
+        {openModal && movie && (
+          <MovieModal {...movie} setOpenModal={setOpenModal} />
+        )}
+      </>
     );
   } else {
     return (
